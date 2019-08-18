7a3c762c784c63667139ebf48f4ffd82c12e45d8
JacobLinCool <jacoblin2011@gmail.com>
2019-08-18 17:07:59 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+console.log("[SW]: Loading WorkBox...");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+if (workbox) {
+    console.log("[SW]: WorkBox Loaded");
+}
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+);
+workbox.routing.registerRoute(
+  /.*\.css/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache',
+  })
+);
+workbox.routing.registerRoute(
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 1 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+workbox.precaching.precacheAndRoute([
+    "/resource/img/background/background.svg",
+    "/resource/img/background/ocean.svg",
+    "/resource/img/background/mountains.svg"
+]);
